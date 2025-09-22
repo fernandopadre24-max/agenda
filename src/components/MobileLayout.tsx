76442d7d20a00a803d0939b804cb660dd49b5583
fernando sena <fernandopadre24@gmail.com>
2025-09-22 23:50:03 +0000

    erro

--- src/components/MobileLayout.tsx
+++ src/components/MobileLayout.tsx
@@ -1,7 +1,7 @@
 export function MobileLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex justify-center bg-gray-100 dark:bg-gray-900">
-      <div className="relative w-full max-w-lg bg-background shadow-2xl min-h-screen flex flex-col sm:overflow-hidden sm:my-4 sm:rounded-lg">
+      <div className="relative w-full max-w-lg bg-background shadow-2xl min-h-screen flex flex-col">
         {children}
       </div>
     </div>
