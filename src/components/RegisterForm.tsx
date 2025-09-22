1a1291a8034fe432cdeb535a401dd21a502c387c
fernando sena <fernandopadre24@gmail.com>
2025-09-22 23:39:02 +0000

    opçao de cadastrar contratante e artistas

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -16,12 +16,16 @@ import {
 import { Input } from '@/components/ui/input';
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 import { useToast } from '@/hooks/use-toast';
+import { RadioGroup, RadioGroupItem } from './ui/radio-group';
 
 const registerFormSchema = z.object({
   name: z.string().min(1, 'O nome é obrigatório.'),
   email: z.string().email('Formato de e-mail inválido.'),
   password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres.'),
   confirmPassword: z.string(),
+  userType: z.enum(['contratante', 'artista'], {
+    required_error: 'Selecione o tipo de usuário.',
+  }),
 }).refine((data) => data.password === data.confirmPassword, {
   message: "As senhas não coincidem.",
   path: ["confirmPassword"],
@@ -56,6 +60,19 @@ export function RegisterForm() {
         <Card>
             <CardHeader><CardTitle className="font-headline">Crie sua conta</CardTitle></CardHeader>
             <CardContent className="space-y-4">
+                <FormField
+                    control={form.control}
+                    name="userType"
+                    render={({ field }) => (
+                        <FormItem className="space-y-3"><FormLabel>Você é...</FormLabel>
+                        <FormControl>
+                            <RadioGroup onValueChange={field.onChange} defaultValue={field.value} className="flex space-x-4">
+                            <FormItem className="flex items-center space-x-2 space-y-0"><FormControl><RadioGroupItem value="contratante" /></FormControl><FormLabel className="font-normal">Contratante</FormLabel></FormItem>
+                            <FormItem className="flex items-center space-x-2 space-y-0"><FormControl><RadioGroupItem value="artista" /></FormControl><FormLabel className="font-normal">Artista</FormLabel></FormItem>
+                            </RadioGroup>
+                        </FormControl><FormMessage /></FormItem>
+                    )}
+                />
                 <FormField control={form.control} name="name" render={({ field }) => (
                     <FormItem><FormLabel>Nome Completo</FormLabel><FormControl><Input placeholder="Seu nome" {...field} /></FormControl><FormMessage /></FormItem>
                 )}/>
